5bbd4cf5ee347bcb3d23a197072de985290829bc
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2024-11-16 10:02:29 +0300

    Time: 0 ms (100%), Space: 17 MB (99.93%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums1=sorted(nums)
+            return nums1.index(target)
+        
