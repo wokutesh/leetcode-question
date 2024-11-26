1ece57fdce52ac42b6818d5454191dc4829a447a
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2024-11-26 10:58:34 +0300

    Time: 451 ms (19.73%), Space: 16.7 MB (32.32%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.py
@@ -1,10 +1,5 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        count=0
-        for i in range(limit+1):
-            for j in range(limit+1):
-                for k in range(limit+1):
-                    if i+j+k==n:
-                        count+=1
-        return count
+       
+        return sum(1 for i in range(limit+1) for j in range(limit+1) for k in range(limit+1) if i+j+k==n)
         
