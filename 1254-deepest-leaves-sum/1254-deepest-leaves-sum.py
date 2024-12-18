544a4513ef8b5533020204a8d88915275614320f
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2024-12-18 09:49:41 +0300

    Time: 48 ms (6.61%), Space: 20.5 MB (8.98%) - LeetHub

---
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return []
+        queue=[root]
+        arr=[]
+        while queue:
+            arr=[]
+            for _ in range(len(queue)):
+
+                curr=queue.pop(0)
+                arr.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)    
+
+        return sum(arr)
