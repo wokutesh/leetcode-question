72b302a76acd860e8cdbfacd64fd196520a4d9bc
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2024-12-26 11:02:58 +0300

    Time: 2 ms (72.02%), Space: 19.9 MB (7.47%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        for key,val in count.items():
+            if val==1:
+                return key
+                break
+                
+        
