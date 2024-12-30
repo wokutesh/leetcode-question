5d29cf0123e729a13239767244b90241000c3f2e
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2024-12-30 22:20:12 +0300

    Time: 27 ms (5.79%), Space: 17.7 MB (10.18%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,19 +1,16 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        count_char='abcdefghijklmnopqrstuvwxyz'
-
-        word='a'
-        while len(word)<k:
-            temp=''
-            for i in word:
-                if i=='z':
-                    temp+='a'
+        count_char = "abcdefghijklmnopqrstuvwxyz"
+        word = "a"
+        
+        while len(word) < k:
+            temp = ""
+            for char in word:
+                if char == "z":
+                    temp += "a"
                 else:
-
-                    
-                    temp+=count_char[(count_char.index(i))+1]
-
-            word+=temp
-
-        return word[k-1]
+                    temp += count_char[count_char.index(char) + 1]
+            word += temp
+        
+        return word[k - 1]
         
