af161f596ef05472e2f46c4b8ff79f45ac3b402d
Wokuma Teshome <135240007+wokutesh@users.noreply.github.com>
2025-01-12 14:09:17 +0300

    Time: 102 ms (60.52%), Space: 18.9 MB (30.84%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        
+        open_count = 0  
+        for i in range(len(s)):
+            if locked[i] == '0' or s[i] == '(':
+                open_count += 1
+            else:  
+                open_count -= 1
+            if open_count < 0:
+                return False 
+
+       
+        close_count = 0  
+        for i in range(len(s) - 1, -1, -1):
+            if locked[i] == '0' or s[i] == ')':
+                close_count += 1
+            else:  
+                close_count -= 1
+            if close_count < 0:
+                return False 
+
+        
+        return True
